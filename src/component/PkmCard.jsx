ffc8c9b878b45c66e8d281846585932e4339f14c
DylanVallejo <dylan1811vallejo@gmail.com>
2022-10-13 22:48:06 -0500

    Responsive data

--- src/component/PkmCard.jsx
+++ src/component/PkmCard.jsx
@@ -13,26 +13,42 @@ const PkmCard =  ({pokemon}) =>{
                         <h3 >{pokemon.name}</h3>
                         <img  alt='Pokemon' className='cardImg' src={pokemon.sprites.front_default}/> 
                         <div> 
-                            <h2>Abilities</h2>
-                            {
-                                pokemon.abilities.map((pkmAbilities,key )=>{
-                                    return(
-                                        <span key={key}>{pkmAbilities.ability.name}<br/></span> 
-                                    )
-                                })
-                            }
+                            <div className="statistics">
+                                <h2>Abilities</h2>
+                                {
+                                    pokemon.abilities.map((pkmAbilities,key )=>{
+                                        return(
+                                            <span key={key}>{pkmAbilities.ability.name}<br/></span> 
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className="statistics">
                                 <h2> Types: </h2>
-                            {
-                                pokemon.types.map((type,key)=>{
-                                    return(
-                    
+                                {
+                                    pokemon.types.map((type,key)=>{
+                                        return(
+                        
+                                                <span key={key}>
+                                                    {type.type.name}<br/>
+                                                </span>
+                                            
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className="statistics">
+                                <h2>Stats</h2>
+                                {
+                                    pokemon.stats.map((stats, key )=>{
+                                        return(
                                             <span key={key}>
-                                                {type.type.name}<br/>
+                                                {stats.stat.name} {stats.base_stat}<br/>
                                             </span>
-                                        
-                                    )
-                                })
-                            }
+                                        )
+                                    })
+                                }
+                            </div>
                         </div>
                     </div>    
                     {/* </Row> */}
